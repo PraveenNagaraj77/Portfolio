8f75154ccc14ea3356267c31d6b8eeb1256eea4f
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2024-10-01 11:06:36 +0530

    new project added productList

--- src/Components/Projects/ProjectData.jsx
+++ src/Components/Projects/ProjectData.jsx
@@ -5,10 +5,30 @@ import mern from '../../assets/banner/mern.png'
 import shoppy from '../../assets/banner/shoppy.png'
 import todo from '../../assets/banner/todo.png'
 import college from '../../assets/banner/college.png'
+import zwiggy from '../../assets/banner/zwiggy_image.png'
+import productList from '../../assets/banner/productList.png'
 
 const ProjectData = [
     {
       id: 1,
+      title: "Food Delivery-Zwiggy",
+      description: "Developed Using MERN Stack , We can order food, Select Quantity, make payment,View Orders and in admin panel we can update food details ,Data is stored in MongoDB ",
+      liveLink: "https://food-delivery-frontend-iqr6.onrender.com/",
+      githubLink: "https://github.com/PraveenNagaraj77/Food-Delivery",
+      image: zwiggy, // Add the URL to the Netflix banner image
+      tags: ["MERN Stack"]
+    },
+    {
+      id: 2,
+      title: "Product Listing",
+      description: "Developed a responsive web application using NextJs. Where Data is fetched from an  API, we can List all  Products Filter By Category & Search products",
+      liveLink: "https://product-listing-sand.vercel.app/",
+      githubLink: "https://github.com/PraveenNagaraj77/ProductListing",
+      image: productList, // Add the URL to the Netflix banner image
+      tags: [""]
+    },
+    {
+      id: 3,
       title: "Netflix Clone",
       description: "Developed a responsive web application using React.js. Integrated with the TMDB API for video content. Implemented authentication using Firebase.",
       liveLink: "https://net-flixx-clone-bypraveen.netlify.app/",
@@ -17,7 +37,7 @@ const ProjectData = [
       tags: ["Fullstack"]
     },
     {
-      id: 2,
+      id: 4,
       title: "Crypto Price Tracker",
       description: "Built a React.js application to display the top 10 cryptocurrencies. Allowed users to search and view individual cryptocurrency data through an integrated API.",
       liveLink: "https://cryptoprice-track-praveen.netlify.app/",
@@ -26,7 +46,7 @@ const ProjectData = [
       tags: [""]
     },
     {
-      id: 3,
+      id: 5,
       title: "MERN CRUD App",
       description: "Created a CRUD application using the MERN stack. Enabled users to add, update, and delete posts with data stored in MongoDB.",
       liveLink: "https://merncrudapp-te7v.onrender.com/",
@@ -35,7 +55,7 @@ const ProjectData = [
       tags: ["MERN Stack"]
     },
     {
-      id: 4,
+      id: 6,
       title: "ShoppyDay E-Commerce Website",
       description: "Developed a responsive e-commerce website with modules for selecting products and categories (men, women, kids). Added features to add products to the cart and generate the total amount. Built with HTML, CSS, JavaScript, and React.js. Backend integration planned for future updates.",
       liveLink: "https://shoppyday.netlify.app/",
@@ -44,7 +64,7 @@ const ProjectData = [
       tags: ["Fullstack"]
     },
     {
-      id: 5,
+      id: 7,
       title: "ToDo-List App",
       description: "Adds task list, update task list, delete task list.",
       liveLink: "https://todolist-app-react7.netlify.app/",
@@ -53,7 +73,7 @@ const ProjectData = [
       tags: [""]
     },
     {
-      id: 6,
+      id: 8,
       title: "Responsive College Website",
       description: "Simple Website using HTML, CSS, JS with Responsive Design.",
       liveLink: "https://college-page.netlify.app/",
